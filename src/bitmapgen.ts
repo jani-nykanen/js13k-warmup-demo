6f8c16493a54438c25dc427bb3d96f0e5d3f9b65
jani-nykanen <jnyknn@gmail.com>
2022-08-09 18:08:17 +0300

    Bitmap generation & test bitmap; not tested yet

---
+++ src/bitmapgen.ts
@@ -0,0 +1,126 @@
+import { Bitmap } from "./canvas";
+
+
+const TILE_WIDTH  = 8;
+const TILE_HEIGHT = 8;
+
+
+export type RGB222LookupTable = Array<number>;
+
+
+export const generateRGB222LookupTable = () : RGB222LookupTable => {
+
+    let out = new Array<number> (64);
+
+    let r : number;
+    let g : number;
+    let b : number;
+
+    for (let c = 0; c < 64; ++ c) {
+
+        r = c >> 4;
+        g = (c % 0b1111) >> 2;
+        b = (c % 0b11);
+
+        out[c*3]     = r * 85;
+        out[c*3 + 1] = g * 85;
+        out[c*3 + 2] = b * 85; 
+    }
+    return out;
+}
+
+
+const getColorIndex = (pixels : Uint8Array, start : number) : number => {
+
+    const INDICES = [0, 1, 2, 3];
+
+    if (pixels[start + 3] < 255)
+        return -1;
+
+    let r = (pixels[start] / 85)     | 0;
+    let g = (pixels[start + 1] / 85) | 0;
+    let b = (pixels[start + 2] / 85) | 0;
+
+    let index = ((r + g + b) / 3) | 0;
+
+    return INDICES[index];
+}
+
+
+const convertChar = (data : ImageData, pixels : Uint8Array, width : number,
+    startx : number, starty : number, endx : number, endy : number,
+    lookup : RGB222LookupTable, palette : Array<number>) : void => {
+
+    let k : number;
+    let p : number;
+    let index : number;
+
+    for (let y = starty; y < endy; ++ y) {
+
+        for (let x = startx; x < endx; ++ x) {
+
+            k = y * width + x;
+            index = getColorIndex(pixels, k*4);
+            if (index < 0) {
+
+                data.data[k*4 + 3] = 0;
+            }
+            else {
+
+                p = palette[index];
+
+                data.data[k*4]     = lookup[p*3];
+                data.data[k*4 + 1] = lookup[p*3 + 1];
+                data.data[k*4 + 2] = lookup[p*3 + 2];
+                data.data[k*4 + 3] = 255;
+            }
+        }
+    }
+}
+
+
+export const convert2BitImageToRGB222 = (bmp : Bitmap, 
+    lookup : RGB222LookupTable, palette : Array<number[]>) : Bitmap => {
+
+    if (bmp == null) 
+        return null;
+
+    let copy = document.createElement("canvas");
+    copy.width = bmp.width;
+    copy.height = bmp.height;
+
+    let ctx = copy.getContext("2d") as CanvasRenderingContext2D;
+    ctx.drawImage(bmp, 0, 0);
+
+    let data = ctx.getImageData(0, 0, bmp.width, bmp.height);
+    let pixels = Uint8Array.from(data.data);
+
+    let w = (bmp.width / TILE_WIDTH)   | 0;
+    let h = (bmp.height / TILE_HEIGHT) | 0;
+
+    for (let j = 0; j < h; ++ j) {
+
+        for (let i = 0; i < w; ++ i) {
+
+            convertChar(data, pixels, bmp.width,
+                i*TILE_WIDTH, j*TILE_HEIGHT, 
+                (i+1)*TILE_WIDTH, (j+1)*TILE_HEIGHT,
+                lookup, palette[j*w + i]);
+        }
+    }
+    ctx.putImageData(data, 0, 0);
+    
+    return copy;
+}
+
+
+export const loadBitmap = (path : string, callback : (bmp : Bitmap) => void) : void => {
+
+    let image = new Image();
+    image.onload = () => {
+
+        callback(image);
+    }
+    image.src = path;
+}
+
